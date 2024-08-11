9423300c01652c46d14289a964dcbe99b9d3c057
Ale Espinosa <espinosacordovaaleff@gmail.com>
2024-08-11 16:55:35 -0600

    Stories V1

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function stories()
+    {
+        return $this->hasMany(Story::class);
+    }
+
+
+    
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/wireui/wireui/resources/**/*.blade.php',
+        './vendor/wireui/wireui/src/View/**/*.php',
+        './vendor/wireui/wireui/ts/**/*.ts',
     ],
 
     theme: {
@@ -17,5 +20,8 @@ export default {
         },
     },
 
+    presets: [
+        require('./vendor/wireui/wireui/tailwind.config.js'),
+    ],
     plugins: [forms],
 };
